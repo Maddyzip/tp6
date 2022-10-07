cd7330d9b6dccc25c598d4af039290171a903098
maddyzip <maddyszip@gmail.com>
2022-10-07 19:21:29 +0000

    add all files

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// JavaScript for TP5
+
+window.onload = function() {
+   
+  document.querySelector("#titleBanner h1").classList.add("tp");
+  
+   document.querySelector("#titleBanner h1").onclick = function() {
+    this.classList.toggle("black");
+  }
+  
+  document.querySelector("#recipeColumns #ingredients").onclick = function() {
+    this.classList.toggle("tpShow");
+  }
+  
+  document.querySelector("#recipeColumns #equipment").onclick = function() {
+    this.classList.toggle("tpShow");
+  }
+  
+  document.querySelector("#recipeColumns #directions").onclick = function() {
+    this.classList.toggle("tpShow");
+  }
+  
+  document.querySelector("#footer").innerHTML += "enjoy this pancake gif";
+} //end of window.onload
